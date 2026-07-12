5149e22f38bde10e3e378c0f846790f2a65408bf
Dev-Studio95 <arnav95012@gmail.com>
2026-07-12 15:41:46 +0530

    Create time-conversion.java

---
+++ 02-Unit-Conversion/time-conversion.java
@@ -0,0 +1,24 @@
+/*
+ * Question: Input total seconds and break it down into hours, minutes, and remaining seconds.
+ * Approach: Divide total seconds by 3600 to find hours. Use the remainder (%) operator 
+ * to track leftover seconds, dividing by 60 to find minutes and capturing the final remainder.
+ */
+import java.util.*;
+
+class time-conversion
+{
+    public static void main() 
+    {
+        Scanner in = new Scanner(System.in);
+        int h, m, s;
+        System.out.println("Enter seconds...");
+        s = in.nextInt();
+        
+        h = s / 3600;
+        s = s % 3600;
+        m = s / 60;
+        s = s % 60;
+        
+        System.out.println(h + "hours" + m + "minutes" + s + "seconds");
+    }
+}
